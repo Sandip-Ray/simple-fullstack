77e1366b3cf288f25b5309f7ab3e0e69dbc43784
Sandip Ray <ipi@stolt.com>
2026-02-25 23:04:45 +0530

    fix url mapping

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
+import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# CORS Configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],       
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -13,3 +16,6 @@ app.add_middleware(
 @app.get("/api/hello")
 def hello():
     return {"message": "Full CI/CD with env tagging working"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
